71b4260aa6ac0fdfae78e68bcedd75172e332f1a
Vectozavr <60608292+vectozavr@users.noreply.github.com>
2024-03-29 13:11:51 +0300

    Implement EvenHandler (take implementation from SFML version that is not type safe, but it works)

--- engine/utils/EventHandler.cpp
+++ engine/utils/EventHandler.cpp
@@ -1,1 +1,14 @@
-#include "EventHandler.h"
+//
+// Created by Ivan Ilin on 26/05/2023.
+//
+
+#include <utils/EventHandler.h>
+
+EventHandler *EventHandler::_instance = nullptr;
+
+void EventHandler::init() {
+    delete _instance;
+    _instance = new EventHandler();
+
+    Log::log("EventHandler::init(): event handler was initialized");
+}
--- engine/utils/EventHandler.h
+++ engine/utils/EventHandler.h
@@ -1,10 +1,91 @@
-#ifndef UTILS_EVENTHANDLER_H
-#define UTILS_EVENTHANDLER_H
+//
+// Created by Ivan Ilin on 26/05/2023.
+//
 
-class EventHandler {
-    // TODO: implement EventHandler. Ideally this would be type safe. I have to think how to make it good...
-    // The solution might be to use SDL_Event: https://wiki.libsdl.org/SDL2/SDL_Event
+#ifndef SHOOTER_EVENTHANDLER_H
+#define SHOOTER_EVENTHANDLER_H
+
+#include <functional>
+#include <utility>
+#include <memory>
+#include <string>
+#include <list>
+#include <map>
+
+#include <utils/Log.h>
+
+class Event final {
+private:
+    std::string s_event_name;
+public:
+    explicit Event(std::string  name) : s_event_name(std::move(name)) {}
+
+    bool operator==(const Event &event) const { return s_event_name == event.s_event_name; }
+    bool operator!=(const Event &event) const { return s_event_name != event.s_event_name; }
+    bool operator<(const Event &event) const { return s_event_name < event.s_event_name; }
+
+    [[nodiscard]] std::string str() const { return s_event_name; }
+};
+
+class Function{};
+
+template<typename Argtype>
+class CallBack final : public Function {
+private:
+    std::function<Argtype> _callBack;
+public:
+    explicit CallBack(const std::function<Argtype>& callBack): _callBack(callBack) {};
+
+    template<typename... Args>
+    void operator()(Args&&... a) {
+        _callBack(a...);
+    }
+};
+
+class EventHandler final {
+private:
+    std::map<Event, std::list<std::unique_ptr<Function>>> _callBacks;
+
+    static EventHandler *_instance;
+
+    EventHandler() = default;
+public:
+    EventHandler(const EventHandler &) = delete;
+
+    EventHandler &operator=(EventHandler &) = delete;
+
+    static void init();
+
+    template<typename Argtype, typename... Arguments>
+    static void call(const Event& event, Arguments... args) {
+        if (_instance == nullptr) {
+            return;
+        }
+        auto functionListIterator = _instance->_callBacks.find(event);
+
+        if(functionListIterator != _instance->_callBacks.end()) {
+            auto& functionList = functionListIterator->second;
+            for (const auto& function : functionList) {
+                auto callBack = *static_cast<CallBack<Argtype>*>(function.get());
+                callBack(args...);
+            }
+        }
+
+        Log::log("EventHandler::call(): event <" + event.str()
+                 + "> happened (" + std::to_string(_instance->_callBacks[event].size()) + " listeners)");
+    }
+
+    template<typename Argtype>
+    static void listen(const Event& event, const std::function<Argtype>& callBack) {
+        if (_instance == nullptr) {
+            return;
+        }
+        _instance->_callBacks[event].emplace_back(std::make_unique<CallBack<Argtype>>(callBack));
+
+        Log::log("EventHandler::listen(): added event listener to <" + event.str()
+        + "> (" + std::to_string(_instance->_callBacks[event].size()) + " listeners)");
+    }
 };
 
 
-#endif //UTILS_EVENTHANDLER_H
+#endif //SHOOTER_EVENTHANDLER_H
